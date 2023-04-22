b0583b3c0938d134be02d5723aeaadd4d23835e8
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-04-22 12:51:34 +0800

    Add tailwind configuration file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "palatinate-blue": '#2B3BE5',
+        "coarse-wool": '#1D1A27',
+        "ghost-white": '#FBF8FF',
+        "pale-marigold": '#FFBE47',
+        "rare-wind": '#5BFBD8',
+        "brave-purple": '#968AB6'
+      },
+      boxShadow: {
+        'purple-sm': '0px 2px 10px rgba(43, 59, 229, 0.2)',
+        'purple-md': '0px 10px 30px rgba(43, 59, 229, 0.3)'
+      },
+      dropShadow: {
+        'purple-sm': '0px 10px 50px rgba(43, 59, 229, 0.08)'
+      },
+      fontFamily: {
+        'outfit': ['Outfit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
