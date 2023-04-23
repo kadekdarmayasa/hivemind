e504c62d2aefb95c69b507c4d43ba4257a414e21
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-04-23 20:37:44 +0800

    Add favicon io

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -10,6 +10,11 @@ export default function Layout({ children, title }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Digital Agency" />
         <meta name="author" content="Darma Yasa" />
+        <link rel="icon" type="image/x-icon" href="/images/favicon_io/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon_io/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <title>{title}</title>
       </Head>
       <div className="xl:container font-outfit mx-auto sm:px-8 xl:px-0">
