efc0b4fe00fc19dd1b232091b7af2ec69ff498bd
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-04-24 09:34:26 +0800

    Add type of parameter

--- pages/api/services/[limit].js
+++ pages/api/services/[limit].js
@@ -1,5 +1,11 @@
 import path from "path";
 import { promises as fs } from 'fs';
+
+/**
+ * 
+ * @param {import("next").NextApiRequest} req 
+ * @param {import("next").NextApiResponse} res 
+ */
 export default async function handler(req, res) {
   try {
     const { limit } = req.query;
--- pages/api/services/index.js
+++ pages/api/services/index.js
@@ -1,5 +1,11 @@
 import path from "path";
 import { promises as fs } from 'fs';
+
+/**
+ * 
+ * @param {import("next").NextApiRequest} req 
+ * @param {import("next").NextApiResponse} res 
+ */
 export default async function handler(req, res) {
   const jsonDirectory = path.join(process.cwd(), 'json');
   const fileContents = await fs.readFile(jsonDirectory + '/services.json', 'utf8');
