d780c3c75d2f70d4b55eefda3ad281f323030c70
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-04-24 09:33:56 +0800

    Adjust card display and add see all service button

--- components/Service/index.js
+++ components/Service/index.js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import useSWR from 'swr';
 import axios from 'axios';
 import Button from '../Button';
-import Link from 'next/link';
 
 const fetcher = (url) => axios.get(url).then(response => response.data);
 
@@ -20,23 +19,27 @@ export default function Service({ limit }) {
         <h2 className="heading-2">Services Provide By Us</h2>
       </div>
 
-      <div className='flex flex-wrap justify-center gap-8 mt-20 rounded-lg'>
+      <div className='flex flex-wrap justify-center gap-8 mt-14'>
         {services.map(service => (
-          <Link href={`service/${service._id}`} className='h-auto block'>
-            <div key={service._id} className='bg-white shadow-black-sm w-[350px] px-10 py-14 flex flex-col items-start'>
-              <div className='bg-palatinate-blue w-20 h-20 flex items-center justify-center rounded-lg mb-6'>
-                <Image src={service.imagePath} height={30} width={30} alt={service.name} style={{ width: '40px' }} />
-              </div>
-              <h3 className='heading-3 mb-3'>{service.name}</h3>
-              <p className='text-brave-purple font-normal text-lg leading-9 mb-5'>{service.briefDescription}</p>
-              <Button type='link' href={`/services/${service._id}`} classNames='place-self-start mt-auto'>
-                <span className='text-lg'>Learn more</span>
-                <Image src='/images/arrow-forward.svg' height={22} width={22} alt='Arrow Forward' className='mt-[2px] ml-1' />
-              </Button>
+          <div key={service._id} className='bg-white shadow-black-sm hover:shadow-black-md hover:scale-[1.02] transition-all w-[360px] h-[500px] px-10 py-14 flex flex-col items-start rounded-xl'>
+            <div className='bg-palatinate-blue w-20 h-20 flex items-center justify-center rounded-lg mb-8'>
+              <Image src={service.imagePath} height={30} width={30} alt={service.name} style={{ width: '40px' }} />
             </div>
-          </Link>
+            <h3 className='heading-3 mb-3'>{service.name}</h3>
+            <p className='text-brave-purple font-normal text-lg leading-9 mb-5'>{service.briefDescription}</p>
+            <Button type='link' href={`/services/${service._id}`} classNames='place-self-start mt-auto relative group'>
+              <span className='text-lg'>Learn more</span>
+              <Image src='/images/arrow-forward.svg' height={22} width={22} alt='Arrow Forward' className='mt-[2px] ml-1 group-hover:ml-2 transition-all' />
+              <div className='absolute h-[2px] rounded-lg w-0 left-0 group-hover:w-full -bottom-1 opacity-0 group-hover:opacity-100 transition-all bg-palatinate-blue'></div>
+            </Button>
+          </div>
         )
         )}
+        <Button type='link' href='/services' classNames='mt-auto mb-10 ml-auto relative group'>
+          <span className='text-lg'>See all services</span>
+          <Image src='/images/arrow-forward.svg' height={22} width={22} alt='Arrow Forward' className='mt-[2px] ml-1 group-hover:ml-2 transition-all' />
+          <div className='absolute h-[2px] rounded-lg w-0 left-0 group-hover:w-full -bottom-1 opacity-0 group-hover:opacity-100 transition-all bg-palatinate-blue'></div>
+        </Button>
       </div>
     </section>
   )
