da4342c82bca4366273bd8241a142b8b379a2843
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-04-27 09:05:25 +0800

    Write unit test for button to test type submit and button

--- components/Button/index.test.js
+++ components/Button/index.test.js
@@ -12,3 +12,15 @@ test('Link Should has background of palatinate blue when it is primary', () => {
 
   expect(container.querySelector('a')).toHaveClass('bg-palatinate-blue');
 })
+
+test('Button should has type submit', () => {
+  const { container } = render(<Button isPrimary type='submit'></Button>);
+
+  expect(container.querySelector('button')).toHaveProperty('type', 'submit');
+})
+
+test('Button should has type button', () => {
+  const { container } = render(<Button isPrimary></Button>);
+
+  expect(container.querySelector('button')).toHaveProperty('type', 'button');
+})
