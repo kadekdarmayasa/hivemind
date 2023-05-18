f065640d50d54ebe1dd057098d337ebb5ef8d527
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-18 21:10:41 +0800

    Refactor code of Blog Component

--- src/components/Blog/BlogCarousel/index.tsx
+++ src/components/Blog/BlogCarousel/index.tsx
@@ -1,14 +1,13 @@
-import Slider from "react-slick";
-import BlogItem from "../BlogItem";
-import type { BlogItemProps } from "types/BlogItem";
-import Fade from 'react-reveal/Fade';
+import Slider from 'react-slick';
+import BlogItem from '../BlogItem';
+import type { BlogItemProps } from 'types/BlogItem';
 
 export default function BlogCarousel({ blogs }: { blogs: BlogItemProps[] }): JSX.Element {
   const settings = {
-    className: "slider variable-width",
+    className: 'slider variable-width',
     infinite: false,
     speed: 1000,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 3000,
     slidesToShow: 3,
     slidesToScroll: 1,
     variableWidth: true,
@@ -18,14 +17,16 @@ export default function BlogCarousel({ blogs }: { blogs: BlogItemProps[] }): JSX
   };
 
   return (
-    <Fade>
-      <div className="h-auto mt-14 mb-20">
-        <Slider {...settings} className="bg-ghost-white h-auto">
-          {blogs.map(blog => (
-            <BlogItem key={blog._id} blog={blog} />
-          ))}
-        </Slider>
-      </div>
-    </Fade>
+    // TODO: Using framer motion for animation
+    <div className={`h-auto mt-14 mb-20`}>
+      <Slider
+        {...settings}
+        className={`bg-ghost-white h-auto`}
+      >
+        {blogs.map((blog) => (
+          <BlogItem key={blog._id} blog={blog} />
+        ))}
+      </Slider>
+    </div>
   )
 }
--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,17 +1,13 @@
 import BlogCarousel from './BlogCarousel/index';
 import type { BlogItemProps } from 'types/BlogItem'
-import Fade from 'react-reveal/Fade';
 
 export default function Blog({ blogs }: { blogs: BlogItemProps[] }) {
   return (
-    <section className="mt-32 2xl:mt-44">
-      <div className="flex flex-col items-center text-center">
-        <Fade bottom>
-          <small className='label-text'>Our Blogs</small>
-        </Fade>
-        <Fade bottom delay={300}>
-          <h2 className="heading-2">Latest Blogs</h2>
-        </Fade>
+    // TODO: Add Framer Motion for Animation
+    <section className='mt-32 2xl:mt-44'>
+      <div className='flex flex-col items-center text-center'>
+        <small className='label-text'>Our Blogs</small>
+        <h2 className='heading-2'>Latest Blogs</h2>
       </div>
 
       <BlogCarousel blogs={blogs} />
