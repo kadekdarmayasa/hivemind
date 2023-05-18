57e41334b557eca6958a4d7172e5f2c9cec91906
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-18 22:32:55 +0800

    Update test button to using screen instead of destructuring from screen

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -1,26 +1,26 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Button from './index';
 
 test('Button Should has background of palatinate blue when it is primary', () => {
-  const { container } = render(<Button isPrimary></Button>);
+  render(<Button isPrimary></Button>);
 
-  expect(container.querySelector('button')).toHaveClass('bg-palatinate-blue');
+  expect(screen.getByRole('button')).toHaveClass('bg-palatinate-blue');
 });
 
 test('Link Should has background of palatinate blue when it is primary', () => {
-  const { container } = render(<Button isPrimary type='link' href='/'></Button>);
+  render(<Button isPrimary type='link' href='/'></Button>);
 
-  expect(container.querySelector('a')).toHaveClass('bg-palatinate-blue');
-})
+  expect(screen.getByRole('link')).toHaveClass('bg-palatinate-blue');
+});
 
 test('Button should has type submit', () => {
-  const { container } = render(<Button isPrimary type='submit'></Button>);
+  render(<Button isPrimary type='submit'></Button>);
 
-  expect(container.querySelector('button')).toHaveProperty('type', 'submit');
-})
+  expect(screen.getByRole('button')).toHaveProperty('type', 'submit');
+});
 
 test('Button should has type button', () => {
-  const { container } = render(<Button isPrimary></Button>);
+  render(<Button isPrimary></Button>);
 
-  expect(container.querySelector('button')).toHaveProperty('type', 'button');
-})
+  expect(screen.getByRole('button')).toHaveProperty('type', 'button');
+});
