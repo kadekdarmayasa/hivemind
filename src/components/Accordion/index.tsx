6de6f70da5cc08e07dc94ba21f0f9e23dcf3267f
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-18 20:44:45 +0800

    Refactor code of Accordion Items and Use React Icons instead of make a custom one

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useState } from 'react';
 import {
   Accordion,
   AccordionHeader,
   AccordionBody
 } from '@material-tailwind/react';
-import Icon from "./Icon";
-import type { FAQProps } from "types/FAQProps";
-import Fade from 'react-reveal/Fade';
+import { IconContext } from 'react-icons';
+import { IoChevronDown } from 'react-icons/io5';
+import type { FAQProps } from 'types/FAQProps';
 
 export default function Index({ faqs }: { faqs: FAQProps[] }): JSX.Element {
-  const [multipleOpen, setMultipleOpen] = useState<number[]>([1])
+  const [multipleOpen, setMultipleOpen] = useState<number[]>([1]);
 
   const handleMultipleOpen = (value: number): void => {
     if (multipleOpen.includes(value)) {
-      let opens = multipleOpen.filter(open => open !== value);
+      const opens = multipleOpen.filter((open) => open !== value);
       setMultipleOpen([...new Set([...opens])]);
     } else {
       setMultipleOpen([...new Set([...multipleOpen, value])]);
@@ -23,16 +23,39 @@ export default function Index({ faqs }: { faqs: FAQProps[] }): JSX.Element {
   return (
     <>
       {faqs.map((faq, index) => (
-        <Fade delay={300 * index} key={faq._id}>
-          <Accordion open={multipleOpen.includes(++index)} icon={<Icon id={index} multipleOpen={multipleOpen} />} className={`bg-white px-6 py-2 mb-6 ${multipleOpen.includes(index) ? 'shadow-black-sm' : ''}`} data-testid="accordion-item">
-            <AccordionHeader onClick={() => handleMultipleOpen(index)} className={`heading-3 font-outfit !font-medium !border-none ${multipleOpen.includes(index) ? '!text-palatinate-blue' : '!text-coarse-wool'}`}>
-              {faq.question}
-            </AccordionHeader>
-            <AccordionBody className="text-brave-purple !pt-2 pb-6 font-outfit font-light text-lg leading-9">
-              {faq.answer}
-            </AccordionBody>
-          </Accordion>
-        </Fade>
+        // TODO: Add Framer Motion for Animation
+        <Accordion
+          key={faq._id}
+          open={multipleOpen.includes(++index)}
+          data-testid={`accordion-item`}
+          icon={
+            <IconContext.Provider value={{ size: '0.8em' }}>
+              <IoChevronDown
+                className={`${multipleOpen.includes(index) ? 'rotate-180' : ''}`}
+              />
+            </IconContext.Provider>
+          }
+          className={
+            `bg-white px-6 py-2 mb-6 
+            ${multipleOpen.includes(index) ? 'shadow-black-sm' : ''}`
+          }
+        >
+
+          <AccordionHeader
+            onClick={() => handleMultipleOpen(index)}
+            className={
+              `heading-3 font-outfit !font-medium !border-none 
+              ${multipleOpen.includes(index) ? '!text-palatinate-blue' : '!text-coarse-wool'}`
+            }
+          >
+            {faq.question}
+          </AccordionHeader>
+
+          <AccordionBody className={`text-brave-purple !pt-2 pb-6 font-outfit font-light text-lg leading-9`}>
+            {faq.answer}
+          </AccordionBody>
+
+        </Accordion>
       ))}
     </>
   )
