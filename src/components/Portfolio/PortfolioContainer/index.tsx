44d2e60266b0178d8bb0bb9d1fc464a2343250b1
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-18 22:12:58 +0800

    Refactoring code of Portfolio Component

--- src/components/Portfolio/PortfolioContainer/index.tsx
+++ src/components/Portfolio/PortfolioContainer/index.tsx
@@ -3,9 +3,9 @@ import PortfolioItem from '../PortfolioItem';
 
 export default function PortfolioContainer({ portfolios }: { portfolios: PortfolioProps[] }): JSX.Element {
   return (
-    <div className='grid grid-cols-12 grid-flow-dense gap-5 mt-14'>
-      {portfolios.map((portfolio, index: number) => (
-        <PortfolioItem key={portfolio._id} index={index} portfolio={portfolio} />
+    <div className={`grid grid-cols-12 grid-flow-dense gap-5 mt-14`}>
+      {portfolios.map((portfolio) => (
+        <PortfolioItem key={portfolio._id} portfolio={portfolio} />
       ))}
     </div>
   )
--- src/components/Portfolio/PortfolioItem/index.tsx
+++ src/components/Portfolio/PortfolioItem/index.tsx
@@ -1,41 +1,73 @@
 import type { PortfolioProps } from "types/Portfolio";
-import Fade from 'react-reveal/Fade';
 import Image from "next/image";
 
-export default function PortfolioItem({ portfolio, index }: { portfolio: PortfolioProps, index: number }): JSX.Element {
+export default function PortfolioItem({ portfolio }: { portfolio: PortfolioProps }): JSX.Element {
+  // TODO: Add framer motion for animation
   return portfolio.orientation === 'potrait' ? (
-    <Fade bottom delay={index * 300}>
-      <div key={portfolio._id} className='rounded-lg relative col-span-4  row-span-4 overflow-hidden group'>
-        <Image width={400} height={600} src={portfolio.imageId} alt={portfolio.projectName} className='h-full w-full object-cover group-hover:scale-105 transition-all' />
+    <div
+      key={portfolio._id}
+      className={`rounded-lg relative col-span-4  row-span-4 overflow-hidden group`}
+    >
+      <Image
+        src={portfolio.imageId}
+        alt={portfolio.projectName}
+        width={400}
+        height={600}
+        className={`h-full w-full object-cover group-hover:scale-105 transition-all`}
+      />
 
-        <div className='absolute h-auto w-auto top-0 bottom-0 right-0 left-0 bg-gradient-to-t from-black to-blue-gray-800 opacity-40 backdrop-blur-md'></div>
+      <div className={
+        `absolute h-auto w-auto top-0 bottom-0 right-0 left-0 bg-gradient-to-t from-black to-blue-gray-800
+        opacity-40 backdrop-blur-md`
+      }></div>
 
-        <div className='absolute bottom-6 group-hover:bottom-8 px-6 transition-all delay-100'>
-          <h3 className='text-2xl text-white font-medium leading-snug mb-2' style={{
-            textShadow: '0 5px 10px rgba(0, 0, 0, .2)',
-          }}>{portfolio.projectName}</h3>
-          <p className='text-white font-normal text-lg' style={{
-            textShadow: '0 5px 10px rgba(0, 0, 0, .2)',
-          }}>{portfolio.service.category}</p>
-        </div>
+      <div className={`absolute bottom-6 group-hover:bottom-8 px-6 transition-all delay-100`}>
+        <h3
+          className={`text-2xl text-white font-medium leading-snug mb-2`}
+          style={{ textShadow: '0 5px 10px rgba(0, 0, 0, .2)' }}
+        >
+          {portfolio.projectName}
+        </h3>
+        <p
+          className={`text-white font-normal text-lg`}
+          style={{ textShadow: '0 5px 10px rgba(0, 0, 0, .2)' }}
+        >
+          {portfolio.service.category}
+        </p>
       </div>
-    </Fade>
+    </div>
   ) : (
-    <Fade bottom delay={index * 300}>
-      <div key={portfolio._id} className='rounded-lg col-span-4 relative row-span-2 overflow-hidden group'>
-        <Image width={400} height={200} src={portfolio.imageId} alt={portfolio.projectName} className='h-full w-full object-cover group-hover:scale-105 transition-all' />
+    <div
+      key={portfolio._id}
+      className={`rounded-lg col-span-4 relative row-span-2 overflow-hidden group`}
+    >
+      <Image
+        src={portfolio.imageId}
+        alt={portfolio.projectName}
+        width={400}
+        height={200}
+        className={`h-full w-full object-cover group-hover:scale-105 transition-all`}
+      />
 
-        <div className='absolute h-auto w-auto top-0 bottom-0 right-0 left-0 bg-gradient-to-t from-black to-blue-gray-800 opacity-40 backdrop-blur-md'></div>
+      <div className={
+        `absolute h-auto w-auto top-0 bottom-0 right-0 left-0 bg-gradient-to-t from-black 
+        to-blue-gray-800 opacity-40 backdrop-blur-md`
+      }></div>
 
-        <div className='absolute bottom-6 group-hover:bottom-8 px-6 transition-all delay-100'>
-          <h3 className='text-xl text-white font-medium leading-snug mb-2' style={{
-            textShadow: '0 5px 10px rgba(0, 0, 0, .2)',
-          }}>{portfolio.projectName}</h3>
-          <p className='text-white font-normal text-base delay-100' style={{
-            textShadow: '0 5px 10px rgba(0, 0, 0, .2)',
-          }}>{portfolio.service.category}</p>
-        </div>
+      <div className={`absolute bottom-6 group-hover:bottom-8 px-6 transition-all delay-100`}>
+        <h3
+          className={`text-xl text-white font-medium leading-snug mb-2`}
+          style={{ textShadow: '0 5px 10px rgba(0, 0, 0, .2)' }}
+        >
+          {portfolio.projectName}
+        </h3>
+        <p
+          className={`text-white font-normal text-base delay-100`}
+          style={{ textShadow: '0 5px 10px rgba(0, 0, 0, .2)' }}
+        >
+          {portfolio.service.category}
+        </p>
       </div>
-    </Fade>
-  )
+    </div>
+  );
 }
