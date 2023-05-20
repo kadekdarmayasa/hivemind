fd16489eb01955b23ed37fec65453415e4e3107f
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-20 22:50:13 +0800

    Add styling for custom underline

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,22 @@
-import Brand from "../Brand";
-import NavigationMenu from "./NavigationMenu";
-import type { NavigationMenuProps } from "types/NavigationMenu";
-import Fade from 'react-reveal/Fade';
+import Brand from '../Brand';
+import NavigationMenu from './NavigationMenu';
+import type { NavigationMenuProps } from 'types/NavigationMenu';
 
 export default function Navbar({ menus }: { menus: NavigationMenuProps[] }): JSX.Element {
+  // TODO: using framer motion for animation
   return (
-    <Fade>
-      <nav className="flex h-[80px] justify-between items-center">
-        <Brand />
-        <ul className="flex items-center">
-          {menus.map((menu, index) => (<NavigationMenu key={index} href={menu.href} name={menu.name} containSubMenu={menu.containSubMenu} subMenu={menu.subMenu} />))}
-        </ul>
-      </nav>
-    </Fade>
+    <nav className='flex h-[80px] justify-between items-center'>
+      <Brand />
+      <ul className='flex items-center'>
+        {menus.map((menu, index) => (
+          <NavigationMenu 
+            key={index} 
+            path={menu.path} 
+            name={menu.name} 
+            containSubMenu={menu.containSubMenu} 
+            subMenus={menu.subMenus} />))}
+      </ul>
+    </nav>
   );
 }
 
