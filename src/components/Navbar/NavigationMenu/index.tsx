acc3f2c294fe6913139b58ac67550b8e9431a80e
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-20 22:48:11 +0800

    Refactor code logic for displaying menu items:

--- src/components/Navbar/NavigationMenu/index.tsx
+++ src/components/Navbar/NavigationMenu/index.tsx
@@ -1,57 +1,125 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { NextRouter, useRouter } from "next/router";
 import { IconContext } from "react-icons";
 import { IoChevronForwardOutline } from "react-icons/io5";
 import type { NavigationMenuProps } from "types/NavigationMenu";
 import { useAppSelector } from "hooks/useAppSelector";
 import { useAppDispatch } from "hooks/useAppDispatch";
 import { selectedState, toggle } from 'slices/dropdownSlice';
+import { AppDispatch } from "store";
 
-export default function NavigationMenu({ href, name, containSubMenu, subMenu }: NavigationMenuProps): JSX.Element {
+export default function NavigationMenu({ path, name, containSubMenu, subMenus }: NavigationMenuProps): JSX.Element {
   const dropdownState = useAppSelector(selectedState);
   const dispatch = useAppDispatch();
   const router = useRouter();
 
-  if (containSubMenu) {
-    return (
-      <li tabIndex={0} className={`z-50 text-base font-regular relative text-brave-purple ml-10 nav-link ${dropdownState === 'open' || router.asPath.split('/')[1] === 'service' ? 'active' : ''}`} onBlur={() => dispatch(toggle('close'))} onFocus={() => dispatch(toggle('open'))} onMouseEnter={() => dispatch(toggle('open'))} onMouseLeave={() => dispatch(toggle('close'))}>
-        <div className="flex items-center">
-          <span className="service-menu-name">{name}</span>
-          <div className="h-[1.2px] w-0 bg-palatinate-blue absolute -bottom-1 left-0 opacity-0 transition-all text-underline"></div>
-          <IconContext.Provider value={{ size: '1.2em', className: 'ms-1 mt-[2px]' }}>
-            <IoChevronForwardOutline className="rotate-90" />
-          </IconContext.Provider>
-        </div>
+  return containSubMenu ? (
+    <MenuWithSubMenu
+      menu={{ name, path }}
+      subMenus={subMenus}
+      dispatch={dispatch}
+      dropdownState={dropdownState}
+      router={router}
+    />
+  ) : (
+    <MenuWithoutSubMenu
+      menu={{ name, path }}
+      router={router}
+    />
+  );
+}
 
-        <ul
-          className={`font-light absolute z-50 w-80 bg-white rounded-lg shadow-black-md mt-4 px-4 py-5 right-2/4 translate-x-2/4 transition-all origin-top ${dropdownState === 'open' ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}`}
+function MenuWithSubMenu({
+  menu,
+  subMenus,
+  dispatch,
+  dropdownState,
+  router,
+}: {
+  menu: {
+    name: NavigationMenuProps['name'],
+    path: NavigationMenuProps['path'],
+  },
+  subMenus: NavigationMenuProps['subMenus'],
+  dispatch: AppDispatch,
+  dropdownState: 'open' | 'close',
+  router: NextRouter
+}) {
+  return (
+    <li
+      tabIndex={0}
+      className={`
+        z-50 text-base font-regular relative text-brave-purple ml-10 nav-link 
+        ${dropdownState === 'open' || router.asPath.split('/')[1] === 'service' ? 'active' : ''}`
+      }
+      onBlur={() => dispatch(toggle('close'))}
+      onFocus={() => dispatch(toggle('open'))}
+      onMouseEnter={() => dispatch(toggle('open'))}
+      onMouseLeave={() => dispatch(toggle('close'))}
+    >
+      <div className='flex items-center'>
+        <span className='service-menu-name'>{menu.name}</span>
+        <div className='custom-underline'></div>
+        <IconContext.Provider value={{ size: '1.2em', className: 'ms-1 mt-[2px]' }}>
+          <IoChevronForwardOutline className="rotate-90" />
+        </IconContext.Provider>
+      </div>
+
+      <ul
+        className={`
+          font-light absolute z-50 w-80 bg-white rounded-lg shadow-black-md mt-4 px-4 py-5 right-2/4 translate-x-2/4 
+          transition-all origin-top ${dropdownState === 'open' ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}
+        `}
+      >
+        {subMenus.map((subMenu, index) => {
+          const path = `${menu.path}${subMenu.path}`;
+
+          return (
+            <li
+              key={index}
+              className={`sub-menu ${index ? 'mt-2' : ''} ${router.asPath === path ? 'active' : ''}`}
+            >
+              <Link
+                href={path}
+                onClick={() => router.push(path)}
+                className='block px-3 py-3 w-full h-full'
+              >
+                {subMenu.name}
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </li>);
+}
+
+function MenuWithoutSubMenu({
+  menu,
+  router,
+}: {
+  menu: {
+    name: NavigationMenuProps['name'],
+    path: NavigationMenuProps['path'],
+  },
+  router: NextRouter,
+}) {
+  return (
+    <li
+      className={`
+        z-50 text-base font-regular relative text-brave-purple ml-10 nav-link 
+        ${router.asPath === menu.path && 'active'}
+      `}
+    >
+      <div>
+        <Link
+          href={menu.path}
+          onClick={() => router.push(menu.path)}
+          className='block'
         >
-          {subMenu.map((item, index) => {
-            const path = `${href}${item.path}`;
-
-            return (
-              <li key={index} className={`sub-menu ${index ? 'mt-2' : ''} ${router.asPath === path ? 'active' : ''}`}>
-                <Link
-                  href={path}
-                  onClick={() => router.push(path)}
-                  className="block px-3 py-3 w-full h-full"
-                >
-                  {item.name}
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      </li>
-    )
-  } else {
-    return (
-      <li className={`z-50 text-base font-regular relative text-brave-purple ml-10 nav-link ${router.asPath === href && 'active'}`}>
-        <div>
-          <Link href={href} onClick={() => router.push(href)} className="block">{name}</Link>
-          <div className="h-[1.2px] w-0 bg-palatinate-blue absolute -bottom-1 left-0 opacity-0 transition-all text-underline"></div>
-        </div>
-      </li>
-    );
-  }
+          {menu.name}
+        </Link>
+        <div className='custom-underline'></div>
+      </div>
+    </li>
+  );
 }
