c5e14c9fd62df55eb930e0f3de1073f264e9d003
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-21 22:00:34 +0800

    Replace Backticks to SingleQuotes and use inferred type for returned value

--- src/components/Homepage/Service/index.tsx
+++ src/components/Homepage/Service/index.tsx
@@ -1,16 +1,16 @@
 import type { ServiceProps } from 'types/Service';
 import { ServiceContainer } from 'components/Service';
 
-export default function Service({ services }: { services: ServiceProps[] }): JSX.Element {
+export default function Service({ services }: { services: ServiceProps[] }) {
   return (
     // TODO: Add framer motion for animation
-    <section className={`mt-32 2xl:mt-44`}>
-      <div className={`flex flex-col items-center text-center`}>
-        <small className={`label-text`}>Our Services</small>
-        <h2 className={`heading-2`}>Services Provide By Us</h2>
+    <section className='mt-32 2xl:mt-44'>
+      <div className='flex flex-col items-center text-center'>
+        <small className='label-text'>Our Services</small>
+        <h2 className='heading-2'>Services Provide By Us</h2>
       </div>
 
       <ServiceContainer services={services} />
     </section>
-  )
+  );
 }
