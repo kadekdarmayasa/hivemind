bc488ab6ba0ea0aa1957261f9dc9db0c664af26f
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-21 21:58:02 +0800

    Replace all backticks to singlequotes and using inferred types for returned value

--- src/components/Homepage/Portfolio/index.tsx
+++ src/components/Homepage/Portfolio/index.tsx
@@ -4,23 +4,23 @@ import { IconContext } from 'react-icons';
 import type { PortfolioProps } from 'types/Portfolio';
 import { PortfolioContainer } from 'components/Portfolio';
 
-export default function Portfolio({ portfolios }: { portfolios: PortfolioProps[] }): JSX.Element {
+export default function Portfolio({ portfolios }: { portfolios: PortfolioProps[] }) {
   return (
     // TODO: Add Framer Motion for Animation
-    <section className={`mt-32 2xl:mt-44`}>
-      <div className={`flex flex-col items-center text-center`}>
-        <small className={`label-text`}>Our Portfolio</small>
-        <h2 className={`heading-2`}>Our Work and Case Study</h2>
+    <section className='mt-32 2xl:mt-44'>
+      <div className='flex flex-col items-center text-center'>
+        <small className='label-text'>Our Portfolio</small>
+        <h2 className='heading-2'>Our Work and Case Study</h2>
       </div>
 
       <PortfolioContainer portfolios={portfolios} />
 
       <Button
-        type={`link`}
-        href={`/portolio`}
-        className={`mt-8 mx-auto relative group w-[170px]`}
+        type='link'
+        href='/portolio'
+        className='mt-8 mx-auto relative group w-[170px]'
       >
-        <span className={`text-lg`}>See all portfolios</span>
+        <span className='text-lg'>See all portfolios</span>
         <IconContext.Provider
           value={{
             size: '1.3em',
@@ -29,11 +29,8 @@ export default function Portfolio({ portfolios }: { portfolios: PortfolioProps[]
           }}>
           <IoArrowForwardSharp />
         </IconContext.Provider>
-        <div className={
-          `absolute h-[2px] rounded-lg w-0 left-0 group-hover:w-full -bottom-1 opacity-0
-          group-hover:opacity-100 transition-all bg-palatinate-blue`}
-        ></div>
+        <div className='absolute h-[2px] rounded-lg w-0 left-0 group-hover:w-full -bottom-1 opacity-0 group-hover:opacity-100 transition-all bg-palatinate-blue'></div>
       </Button>
     </section>
-  )
+  );
 }
