7a5276c068409a94c3e33b60228455039e07e021
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-21 21:55:31 +0800

    Remove all backticks and use single quote instead, and also add TODO for next iteration

--- src/components/Homepage/OurValues/index.tsx
+++ src/components/Homepage/OurValues/index.tsx
@@ -10,33 +10,28 @@ export default function OurValues({
 }: {
   refOurValues: LegacyRef<HTMLElement>,
   ourValues: OurValuesProps[]
-}): JSX.Element {
+}) {
+  // TODO : Using framer motion for animation
   return (
-    <section className={`mt-44`} ref={refOurValues}>
-      <div className={`flex flex-col items-center text-center`}>
-        <small className={`label-text`}>Our Values</small>
-        <h2 className={`heading-2`}>What Sets Us Apart</h2>
+    <section className='mt-44' ref={refOurValues}>
+      <div className='flex flex-col items-center text-center'>
+        <small className='label-text'>Our Values</small>
+        <h2 className='heading-2'>What Sets Us Apart</h2>
       </div>
 
-      <div className={`flex justify-center mt-14 flex-wrap gap-10`}>
+      <div className='flex justify-center mt-14 flex-wrap gap-10'>
         {ourValues.map((ourValue) => (
-          <div key={ourValue._id} className={`our-values-item`}>
-            <div className={`flex items-center`}>
-              <h3 className={`our-values-item__heading`}>{ourValue.value}</h3>
-              <IconContext.Provider
-                value={{
-                  size: '2em',
-                  color: '#5BFBD8',
-                  className: '-mt-1'
-                }}
-              >
+          <div key={ourValue._id} className='our-values-item'>
+            <div className='flex items-center'>
+              <h3 className='our-values-item__heading'>{ourValue.value}</h3>
+              <IconContext.Provider value={{ size: '2em', color: '#5BFBD8', className: '-mt-1' }}>
                 <IoCheckmarkCircleOutline />
               </IconContext.Provider>
             </div>
-            <p className={`our-values-item__description`}>{parse(ourValue.description)}</p>
+            <p className='our-values-item__description'>{parse(ourValue.description)}</p>
           </div>
         ))}
       </div>
     </section>
-  )
+  );
 }
