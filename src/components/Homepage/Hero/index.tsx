7c881831fc670343e078d3a9aa00298b0663ff4a
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-21 21:45:01 +0800

    Replace backticks to quote string

--- src/components/Homepage/Hero/index.tsx
+++ src/components/Homepage/Hero/index.tsx
@@ -6,7 +6,7 @@ import { useAppSelector } from 'hooks/useAppSelector';
 import { selectedState } from 'slices/dropdownSlice';
 import Image from 'next/image';
 
-export default function Hero({ refOurValues }: { refOurValues: MutableRefObject<HTMLElement> }): JSX.Element {
+export default function Hero({ refOurValues }: { refOurValues: MutableRefObject<HTMLElement> }) {
   const dropdownState = useAppSelector(selectedState);
 
   const showOurValues = () => {
@@ -18,66 +18,50 @@ export default function Hero({ refOurValues }: { refOurValues: MutableRefObject<
 
   return (
     // TODO: Add framer motion for animation
-    <section
-      className={
-        `flex relative mt-16 2xl:mt-24 
-        ${dropdownState === 'open' ? '-z-10' : 'z-0'}`
-      }
-    >
-      <div className={`flex-1 flex flex-col justify-center items-start`}>
-        <small className={`label-text`}>A innovate digital agency</small>
-        <h1 className={`heading-1 mt-1`}>
+    <section className={`flex relative mt-16 2xl:mt-24 ${dropdownState === 'open' ? '-z-10' : 'z-0'}`}>
+      <div className='flex-1 flex flex-col justify-center items-start'>
+        <small className='label-text'>A innovate digital agency</small>
+        <h1 className='heading-1 mt-1'>
           Transform Your Online Presence <br />
           with Our Innovate Digital <br />
           Solutions
         </h1>
-        <p className={`text-brave-purple font-normal text-xl leading-9 mt-3`}>
+        <p className='text-brave-purple font-normal text-xl leading-9 mt-3'>
           We provide personalized strategies that are tailored to <br />
           your business goals, using cutting-edge technology <br />
           and industry best practices
         </p>
-        <div className={`flex mt-12`}>
+        <div className='flex mt-12'>
           <Button
-            type={`link`}
-            href={`/contact`}
+            type='link'
+            href='/contact'
             isPrimary
-            className={`h-[60px] w-[210px] rounded-full hover:shadow-purple-md transition-all`}
+            className='h-[60px] w-[210px] rounded-full hover:shadow-purple-md transition-all'
           >
-            <IconContext.Provider
-              value={{
-                size: '1.6em',
-                className: 'mr-2',
-              }}
-            >
-              <IoChatboxOutline className={`h-10`} />
+            <IconContext.Provider value={{ size: '1.6em', className: 'mr-2' }}>
+              <IoChatboxOutline className='h-10' />
             </IconContext.Provider>
-            <span className={`text-lg`}>Get in Touch</span>
+            <span className='text-lg'>Get in Touch</span>
           </Button>
-          <Button
-            className={`h-[60px] w-[210px] ml-6 relative group`}
-            onClick={showOurValues}
-          >
-            <div className={`rounded-xl h-8 w-5 border-palatinate-blue border-[2px] relative flex justify-center mr-4`}>
-              <span className={`animate-bounce block w-1 h-2 rounded-lg top-2 bg-palatinate-blue absolute`}></span>
+          <Button className='h-[60px] w-[210px] ml-6 relative group' onClick={showOurValues}>
+            <div className='rounded-xl h-8 w-5 border-palatinate-blue border-[2px] relative flex justify-center mr-4'>
+              <span className='animate-bounce block w-1 h-2 rounded-lg top-2 bg-palatinate-blue absolute'></span>
             </div>
-            <span className={`text-lg underline-offset-8`}>Scroll to read more</span>
-            <div className={
-              `absolute h-[2px] w-0 opacity-0 left-0 group-hover:opacity-100 group-hover:w-full transition-all 
-              bg-palatinate-blue bottom-1`}
-            ></div>
+            <span className='text-lg underline-offset-8'>Scroll to read more</span>
+            <div className='absolute h-[2px] w-0 opacity-0 left-0 group-hover:opacity-100 group-hover:w-full transition-all bg-palatinate-blue bottom-1'></div>
           </Button>
         </div>
       </div>
 
-      <div className={`flex-1 flex justify-end items-center`}>
+      <div className='flex-1 flex justify-end items-center'>
         <Image
-          src={`/images/hero-image-1.png`}
-          alt={`Our Team meeting With Client`}
+          src='/images/hero-image-1.png'
+          alt='Our Team meeting With Client'
           width={600}
           height={500}
-          className={`w-[98%] shadow-black-md`}
+          className='w-[98%] shadow-black-md'
         />
       </div>
     </section>
-  )
+  );
 }
