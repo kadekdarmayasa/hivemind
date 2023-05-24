37ef3de991a3173e0b23d61689a9b17cc72039d6
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-24 13:20:59 +0800

    Add partials path aliasses and tailwind config for partials folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const withMT = require('@material-tailwind/react/utils/withMT');
+const TailwindElement = require('tw-elements/dist/plugin.cjs');
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
@@ -7,7 +8,8 @@ module.exports = withMT({
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/pages/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/tw-elements/dist/js/**/*.js'
+    './src/partials/**/*.{js,ts,jsx,tsx}',
+    './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
     extend: {
@@ -17,22 +19,21 @@ module.exports = withMT({
         'ghost-white': '#FBF8FF',
         'pale-marigold': '#FFBE47',
         'rare-wind': '#5BFBD8',
-        'brave-purple': '#968AB6'
+        'brave-purple': '#968AB6',
       },
       boxShadow: {
         'purple-sm': '0px 2px 10px rgba(43, 59, 229, 0.2)',
         'purple-md': '0px 10px 30px rgba(43, 59, 229, 0.3)',
         'black-sm': '0px 2px 30px rgba(0, 0, 0, 0.04)',
-        'black-md': '0px 5px 50px rgba(0, 0, 0, 0.05)'
+        'black-md': '0px 5px 50px rgba(0, 0, 0, 0.05)',
       },
       dropShadow: {
-        'purple-sm': '0px 10px 50px rgba(43, 59, 229, 0.08)'
+        'purple-sm': '0px 10px 50px rgba(43, 59, 229, 0.08)',
       },
       fontFamily: {
-        'sans': ['var(--font-outfit)', ...fontFamily.sans],
+        sans: ['var(--font-outfit)', ...fontFamily.sans],
       },
     },
   },
-  plugins: [require('tw-elements/dist/plugin.cjs')],
+  plugins: [TailwindElement],
 });
-
