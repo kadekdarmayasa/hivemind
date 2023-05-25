d1e0ca4cc5e17ddbfaed633b6bf29a5be89cb3d8
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-25 10:00:25 +0800

    Moving Test Files and add atribute data-testid for test purpose

--- src/components/Star/index.tsx
+++ src/components/Star/index.tsx
@@ -6,18 +6,18 @@ export default function Star({ value, height, width }: StarProps): JSX.Element {
 
   for (let index = 0; index < value - decimals; index++) {
     leftPosition += width;
-    star.push(<div className="star" key={`star-${index}`} style={{ left: index * width, height: height, width: width }}></div>)
+    star.push(<div className="star" data-testid="yellowStar" key={`star-${index}`} style={{ left: index * width, height: height, width: width }}></div>)
   }
 
-  if (decimals > 0 && value <= 5) star.push(<div className="star" key={`starWithDecimal`} style={{ left: leftPosition, height: height, width: decimals * width }}></div>)
+  if (decimals > 0 && value <= 5) star.push(<div className="star" data-testid="yellowStar" key={`starWithDecimal`} style={{ left: leftPosition, height: height, width: decimals * width }}></div>)
 
   for (let index = 0; index < 5; index++) {
-    starPlaceholder.push(<div className="star placeholder" key={`star-placeholder-${index}`} style={{ left: index * width, height: height, width: width }}></div>)
+    starPlaceholder.push(<div className="star placeholder" data-testid="starPlaceholder" key={`star-placeholder-${index}`} style={{ left: index * width, height: height, width: width }}></div>)
   }
 
   return (
     <>
-      <div className={'stars'} style={{ height: height }}>
+      <div className={'stars'} data-testid="stars" style={{ height: height }}>
         {starPlaceholder}
         {star}
       </div>
