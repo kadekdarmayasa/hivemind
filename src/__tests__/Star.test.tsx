cacf6ddfa355c4f2ac22de682f13e108e2cd0647
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-25 10:01:07 +0800

    Refactoring code of Test Star

---
+++ src/__tests__/Star.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Star from '@components/Star';
+
+test("Star's style should have the values of the given width and height", () => {
+  const height = 40;
+  const width = 40;
+
+  render(<Star width={width} height={height} value={4} />);
+
+  const starsContainer = screen.getByTestId('stars');
+  const yellowStars = screen.getAllByTestId('yellowStar');
+  const starPlaceholders = screen.getAllByTestId('starPlaceholder');
+
+  expect(starsContainer).toBeInTheDocument();
+  expect(starsContainer).toHaveAttribute('style', expect.stringContaining(`height: ${height}px`));
+
+  starPlaceholders.forEach((starPlaceholder) => {
+    expect(starPlaceholder).toBeInTheDocument();
+    expect(starPlaceholder).toHaveAttribute('style', expect.stringContaining(`width: ${width}px`));
+    expect(starPlaceholder).toHaveAttribute('style', expect.stringContaining(`height: ${height}px`));
+  });
+
+  yellowStars.forEach((yellowStar) => {
+    expect(yellowStar).toBeInTheDocument();
+    expect(yellowStar).toHaveAttribute('style', expect.stringContaining(`width: ${width}px`));
+    expect(yellowStar).toHaveAttribute('style', expect.stringContaining(`height: ${height}px`));
+  });
+});
