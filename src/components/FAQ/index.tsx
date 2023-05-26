0d46233e325558f230389d44bdcfd5b7576a3fe1
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-26 12:21:00 +0800

    Refactor code of FAQ Component

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -1,17 +1,16 @@
+import React from 'react';
 import Accordion from '@components/Accordion';
 import type { FAQProps } from 'types/FAQProps';
-import Fade from 'react-reveal/Fade';
 
 export default function FAQ({ faqs }: { faqs: FAQProps[] }) {
+  // TODO: Using Framer Motion for Animation
   return (
-    <section className='mt-32'>
-      <Fade up>
-        <h2 className='heading-2 text-center mb-14'>Frequently Asked Questions</h2>
-      </Fade>
+    <section className="mt-32">
+      <h2 className="heading-2 text-center mb-14">Frequently Asked Questions</h2>
 
-      <div className='max-w-[1080px] mx-auto'>
+      <div className="max-w-[1080px] mx-auto">
         <Accordion faqs={faqs} />
       </div>
     </section>
-  )
+  );
 }
