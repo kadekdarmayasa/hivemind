2ce80b1530be6039150b3f979c19243246899f62
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-26 12:38:10 +0800

    Refactor code of Blog Component

---
+++ src/components/Blog/BlogItem.tsx
@@ -0,0 +1,60 @@
+import React, { useMemo } from 'react';
+import Button from '@components/Button';
+import { IconContext } from 'react-icons';
+import { IoArrowForwardSharp } from 'react-icons/io5';
+import type { BlogItemProps } from 'types/BlogItem';
+import Image from 'next/image';
+
+export default function BlogItem({ blog }: { blog: BlogItemProps }) {
+  const iconProps = useMemo(() => ({
+    size: '1.3em',
+    className: 'mt-[2px] ml-1 group-hover:ml-2 transition-all',
+    color: '#2B3BE5',
+  }), []);
+
+  return (
+    <div className="h-[auto] w-[360px] bg-white rounded-xl relative blog-item">
+      <div className="h-[170px] w-full overflow-hidden rounded-t-xl">
+        <Image
+          src={blog.imageId}
+          alt={blog.title}
+          width={300}
+          height={200}
+          className="h-full w-full object-cover"
+        />
+      </div>
+
+      <div className="px-6 py-6">
+        <div className="flex">
+          <small className="text-palatinate-blue text-sm text-center mr-4 rounded-full">
+            {blog.publishedDate}
+          </small>
+          <small className="text-palatinate-blue text-sm text-center rounded-full">
+            {blog.author}
+          </small>
+        </div>
+
+        <h3 className="heading-3 mt-6">{blog.title}</h3>
+
+        <p className="text-brave-purple max-w-full font-light text-lg leading-8 mt-3 mb-16">
+          {blog.briefDescription}
+        </p>
+
+        <Button
+          type="link"
+          href={`/blog/${blog.id}`}
+          className="absolute bottom-8 group"
+        >
+          <span className="text-lg">Read more</span>
+          <IconContext.Provider value={iconProps}>
+            <IoArrowForwardSharp />
+          </IconContext.Provider>
+          <div
+            className="absolute h-[2px] rounded-lg w-0 left-0 group-hover:w-full -bottom-1
+            opacity-0 group-hover:opacity-100 transition-all bg-palatinate-blue"
+          />
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,16 +1,37 @@
-import BlogCarousel from './BlogCarousel'
-import type { BlogItemProps } from 'types/BlogItem'
+import React from 'react';
+import type { BlogItemProps } from 'types/BlogItem';
+import Slider from 'react-slick';
+import BlogItem from './BlogItem.tsx';
 
 export default function Blog({ blogs }: { blogs: BlogItemProps[] }) {
+  const settings = {
+    className: 'slider variable-width',
+    infinite: false,
+    speed: 1000,
+    autoplaySpeed: 3000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+    pauseOnHover: true,
+    slidesPerRow: 1,
+    rows: 1,
+  };
+
   return (
     // TODO: Add Framer Motion for Animation
-    <section className='mt-32 2xl:mt-44'>
-      <div className='flex flex-col items-center text-center'>
-        <small className='label-text'>Our Blogs</small>
-        <h2 className='heading-2'>Latest Blogs</h2>
+    <section className="mt-32 2xl:mt-44">
+      <div className="flex flex-col items-center text-center">
+        <small className="label-text">Our Blogs</small>
+        <h2 className="heading-2">Latest Blogs</h2>
       </div>
 
-      <BlogCarousel blogs={blogs} />
+      <div className="h-auto mt-14 mb-20">
+        <Slider {...settings} className="bg-ghost-white h-auto">
+          {blogs.map((blog) => (
+            <BlogItem key={blog.id} blog={blog} />
+          ))}
+        </Slider>
+      </div>
     </section>
-  )
+  );
 }
