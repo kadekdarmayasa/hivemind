1ef5e1b0dbd2a1ae4a2bcabb7a498e3d770ca63e
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-27 12:53:12 +0800

    Update: Add blog section

---
+++ src/partials/Homepage/Blog.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import type { BlogItemProps } from 'types/BlogItem';
+import BlogCarousel from '@components/Blog';
+
+export default function Blog({ blogs }: { blogs: BlogItemProps[] }) {
+  return (
+    // TODO: Add Framer Motion for Animation
+    <section className="mt-32 2xl:mt-44">
+      <div className="flex flex-col items-center text-center">
+        <small className="label-text">Our Blogs</small>
+        <h2 className="heading-2">Latest Blogs</h2>
+      </div>
+
+      <BlogCarousel blogs={blogs} />
+    </section>
+  );
+}
--- src/partials/Homepage/index.ts
+++ src/partials/Homepage/index.ts
@@ -1,5 +1,6 @@
 export { default as Hero } from './Hero.tsx';
 export { default as OurValues } from './OurValues.tsx';
 export { default as Service } from './Service/index.tsx';
-export { default as Testimony } from './Testimony/index.tsx';
 export { default as Portfolio } from './Portfolio.tsx';
+export { default as Testimony } from './Testimony/index.tsx';
+export { default as Blog } from './Blog.tsx';
