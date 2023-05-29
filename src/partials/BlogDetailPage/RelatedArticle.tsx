6178c53dec34e6c15c2510518d94942dff97a5b7
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-29 10:42:57 +0800

    Update: Refactor code of BlogDetailPage's partials

--- src/partials/BlogDetailPage/RelatedArticle.tsx
+++ src/partials/BlogDetailPage/RelatedArticle.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import type { BlogItemProps } from 'types/BlogItem';
-import BlogCarousel from '@components/Blog/BlogCarousel';
+import Blog from '@components/Blog';
 
-export default function RelatedArticle({ relatedArticles }: { relatedArticles: BlogItemProps[] }) {
+export default function RelatedArticle({
+  relatedArticles,
+}: {
+  relatedArticles: BlogItemProps[];
+}) {
   return (
-    <div className="mt-16">
+    <section className="mt-16">
       <h3 className="heading-3 -mb-10">Related Articles</h3>
-
-      <BlogCarousel blogs={relatedArticles} />
-    </div>
+      <Blog blogs={relatedArticles} />
+    </section>
   );
 }
