aa029a9461939f9afb7f68c89a414012258a24b5
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-05-29 11:07:56 +0800

    Update: Refactoring code

--- src/components/Blog/BlogItem.tsx
+++ src/components/Blog/BlogItem.tsx
@@ -23,92 +23,44 @@ export default function BlogItem({
     [],
   );
 
-  if (isGridItem) {
-    if (contentWidth === 'large') {
-      return (
-        <div className="flex gap-10 w-full col-span-12">
-          <div className="flex flex-1">
-            <Image
-              width={500}
-              height={350}
-              src={blog.imageId}
-              alt={blog.title}
-              className="w-full h-full rounded-lg"
-            />
-          </div>
-
-          <div className="flex flex-1 flex-col items-start justify-center">
-            <div className="flex">
-              <small className="text-palatinate-blue text-sm mr-4 rounded-full">
-                {blog.publishedDate}
-              </small>
-              <small className="text-palatinate-blue text-sm rounded-full">
-                {blog.author}
-              </small>
-            </div>
-
-            <h1 className="heading-1">{blog.title}</h1>
-            <p className="text-brave-purple font-normal text-xl leading-9 mt-3">
-              {blog.briefDescription}
-            </p>
-
-            <Button
-              type="link"
-              href={`/blog/${blog.id}`}
-              className="relative group mt-6"
-            >
-              <span className="text-lg">Read more</span>
-              <IconContext.Provider value={iconProps}>
-                <IoArrowForwardSharp />
-              </IconContext.Provider>
-              <div className="absolute h-[2px] rounded-lg w-0 left-0 group-hover:w-full -bottom-1 opacity-0 group-hover:opacity-100 transition-all bg-palatinate-blue" />
-            </Button>
-          </div>
-        </div>
-      );
-    }
-
+  if (isGridItem && contentWidth === 'large') {
     return (
-      <div className="h-[auto] col-span-4 bg-white rounded-xl relative blog-item">
-        <div className="h-[170px] w-full overflow-hidden rounded-t-xl">
+      <div className="flex gap-10 w-full col-span-12">
+        <div className="flex flex-1">
           <Image
+            width={500}
+            height={350}
             src={blog.imageId}
             alt={blog.title}
-            width={300}
-            height={200}
-            className="h-full w-full object-cover"
+            className="w-full h-full rounded-lg"
           />
         </div>
 
-        <div className="px-6 py-10">
+        <div className="flex flex-1 flex-col items-start justify-center">
           <div className="flex">
-            <small className="text-palatinate-blue text-sm text-center mr-4 rounded-full">
+            <small className="text-palatinate-blue text-sm mr-4 rounded-full">
               {blog.publishedDate}
             </small>
-            <small className="text-palatinate-blue text-sm text-center rounded-full">
+            <small className="text-palatinate-blue text-sm rounded-full">
               {blog.author}
             </small>
           </div>
 
-          <h3 className="heading-3 mt-6">{blog.title}</h3>
-
-          <p className="text-brave-purple max-w-full font-light text-lg leading-8 mt-3 mb-16">
+          <h1 className="heading-1">{blog.title}</h1>
+          <p className="text-brave-purple font-normal text-xl leading-9 mt-3">
             {blog.briefDescription}
           </p>
 
           <Button
             type="link"
             href={`/blog/${blog.id}`}
-            className="absolute bottom-10 group"
+            className="relative group mt-6"
           >
             <span className="text-lg">Read more</span>
             <IconContext.Provider value={iconProps}>
               <IoArrowForwardSharp />
             </IconContext.Provider>
-            <div
-              className="absolute h-[2px] rounded-lg w-0 left-0 group-hover:w-full -bottom-1
-              opacity-0 group-hover:opacity-100 transition-all bg-palatinate-blue"
-            />
+            <div className="absolute h-[2px] rounded-lg w-0 left-0 group-hover:w-full -bottom-1 opacity-0 group-hover:opacity-100 transition-all bg-palatinate-blue" />
           </Button>
         </div>
       </div>
@@ -116,7 +68,11 @@ export default function BlogItem({
   }
 
   return (
-    <div className="h-[auto] w-[360px] bg-white rounded-xl relative blog-item">
+    <div
+      className={`h-[auto] bg-white rounded-xl relative blog-item ${
+        isGridItem ? 'col-span-4' : 'w-[360px]'
+      }`}
+    >
       <div className="h-[170px] w-full overflow-hidden rounded-t-xl">
         <Image
           src={blog.imageId}
