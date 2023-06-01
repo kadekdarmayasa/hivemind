f0300f85e91b1c897db1420dd24f2a48212aaf0c
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-06-01 15:34:16 +0800

    Add Framer Animation to Brand Component

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 export default function Brand() {
   return (
-    <Link
-      href="/"
-      className="text-palatinate-blue text-3xl font-bold flex items-center"
+    <motion.div
+      initial={{ translateY: 100, opacity: 0 }}
+      animate={{ translateY: 0, opacity: 1 }}
+      transition={{ duration: 1.2 }}
     >
-      <Image
-        src="/images/hivemind-logo.svg"
-        alt="Hivemind Logo"
-        height={40}
-        width={40}
-        className="mr-2"
-      />
-      <span>Hivemind</span>
-    </Link>
+      <Link href="/" className="text-palatinate-blue text-3xl font-bold flex items-center">
+        <Image
+          src="/images/hivemind-logo.svg"
+          alt="Hivemind Logo"
+          height={40}
+          width={40}
+          className="mr-2"
+        />
+        <span>Hivemind</span>
+      </Link>
+    </motion.div>
   );
 }
