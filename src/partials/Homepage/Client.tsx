c1bda95f0efd32f82608f29fe982138972579506
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-06-05 09:54:25 +0800

    Add Framer Animation to Client component in Homepage partials

--- src/partials/Homepage/Client.tsx
+++ src/partials/Homepage/Client.tsx
@@ -1,21 +1,69 @@
-import React from 'react';
+import React, { Ref } from 'react';
+import { motion, Variants } from 'framer-motion';
 import type { ClientProps } from 'types/Client';
 import Image from 'next/image';
 
-export default function Client({ clients }: { clients: ClientProps[] }) {
+export default function Client({
+  clients,
+  refClients,
+}: {
+  clients: ClientProps[];
+  refClients: Ref<HTMLElement>;
+}) {
+  const clientVariants: Variants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        ease: 'linear',
+        staggerChildren: 1,
+      },
+    },
+  };
+
+  const clientInnerVariants: Variants = {
+    hidden: { y: 120, opacity: 0 },
+    visible: (i) => ({
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: i * 0.3,
+        duration: 0.3,
+        ease: 'easeInOut',
+      },
+    }),
+  };
+
   return (
-    <section className="mt-32 2xl:mt-44">
-      <div className="flex relative flex-wrap justify-center gap-10">
-        {clients.map((client) => (
-          <Image
+    <motion.section
+      initial="hidden"
+      whileInView="visible"
+      variants={clientVariants}
+      viewport={{ once: true }}
+      className="mt-32 2xl:mt-44"
+      ref={refClients}
+    >
+      <h2 className="heading-2 text-center">
+        Trusted by&nbsp;
+        <span className="text-palatinate-blue">4K+</span>
+        &nbsp;Clients and Leading Brands
+      </h2>
+
+      <div className="flex relative flex-wrap justify-center gap-10 mt-14">
+        {clients.map((client, i) => (
+          <motion.div
+            custom={i}
+            initial="hidden"
+            whileInView="visible"
+            variants={clientInnerVariants}
+            viewport={{ once: true }}
             key={client.id}
-            src={client.imagePath}
-            alt={client.name}
-            width={80}
-            height={80}
-          />
+            className="w-[80px] h-[80px]"
+          >
+            <Image src={client.imagePath} alt={client.name} width={80} height={80} />
+          </motion.div>
         ))}
       </div>
-    </section>
+    </motion.section>
   );
 }
