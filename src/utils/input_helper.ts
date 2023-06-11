fa2334ece55f95990d713a61aee0b375a23b0acc
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-06-11 09:37:42 +0800

    Refactor: use regex as container of validator regex

--- src/utils/input_helper.ts
+++ src/utils/input_helper.ts
@@ -1,23 +1,28 @@
+const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const TEL_REGEX = /[0-9]*/;
+
 type InputHelperProps = {
   validator?: {
-    email?: RegExp,
-    tel?: RegExp,
-  },
+    email?: RegExp;
+    tel?: RegExp;
+  };
 
   errorMessage?: {
-    email: string,
-    text: (name: string) => string
-  }
-}
+    email: string;
+    text: (name: string) => string;
+    default: string;
+  };
+};
 
 export const InputHelper: InputHelperProps = {
   validator: {
-    email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-    tel: /[0-9]*/,
+    email: EMAIL_REGEX,
+    tel: TEL_REGEX,
   },
 
   errorMessage: {
     email: 'Please enter email in a valid format',
     text: (name: string) => `${name} cannot be empty`,
+    default: 'The field cannot be empty',
   },
 };
