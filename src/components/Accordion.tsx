44f14600b0a017b2714b41eb20578a5e4bbec490
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-06-13 09:30:12 +0800

    Refactor code of Accordion Component

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,83 +1,46 @@
-import React, { useState, useMemo } from 'react';
-import { motion, Variants } from 'framer-motion';
-import {
-  Accordion as RCAccordion,
-  AccordionHeader,
-  AccordionBody,
-} from '@material-tailwind/react';
+import { useState } from 'react';
+import { Accordion as RCAccordion, AccordionHeader, AccordionBody } from '@material-tailwind/react';
 import { IconContext } from 'react-icons';
 import { IoChevronDown } from 'react-icons/io5';
 import type { FAQProps } from 'types/FAQProps';
 
-export default function Accordion({ faqs }: { faqs: FAQProps[] }) {
-  const [multipleOpen, setMultipleOpen] = useState([1]);
-  const iconProps = useMemo(() => ({ size: '0.8em' }), []);
+const iconChevronDownProps: IconContext = { size: '0.8em' };
 
-  const transformVariants: Variants = {
-    hidden: { y: 120, opacity: 0 },
-    visible: (i = 0) => ({
-      opacity: 1,
-      y: 0,
-      transition: {
-        delay: i * 0.3,
-        duration: 0.3,
-        ease: 'linear',
-      },
-    }),
-  };
+export default function Accordion({ faqs }: { faqs: FAQProps[] }): JSX.Element {
+  const [openStates, setOpenStates] = useState<boolean[]>(faqs.map(() => false));
 
-  const handleMultipleOpen = (value: number): void => {
-    if (multipleOpen.includes(value)) {
-      const opens = multipleOpen.filter((open) => open !== value);
-      setMultipleOpen([...new Set([...opens])]);
-    } else {
-      setMultipleOpen([...new Set([...multipleOpen, value])]);
-    }
+  const handleToggleOpen = (index: number): void => {
+    const updatedOpenStates = [...openStates];
+    updatedOpenStates[index] = !updatedOpenStates[index];
+    setOpenStates(updatedOpenStates);
   };
 
   return (
     <>
       {faqs.map((faq, index) => (
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          variants={transformVariants}
-          custom={index}
-          key={faq.id}
+        <RCAccordion
+          open={openStates[index]}
+          data-testid="accordion-item"
+          icon={
+            <IconContext.Provider value={iconChevronDownProps}>
+              <IoChevronDown className={`${openStates[index] ? 'rotate-180' : ''}`} />
+            </IconContext.Provider>
+          }
+          className={`bg-white px-6 py-2 mb-6 
+            ${openStates[index] ? 'shadow-black-sm' : ''}`}
         >
-          <RCAccordion
-            open={multipleOpen.includes(++index)}
-            data-testid="accordion-item"
-            icon={
-              <IconContext.Provider value={iconProps}>
-                <IoChevronDown
-                  className={`${
-                    multipleOpen.includes(index) ? 'rotate-180' : ''
-                  }`}
-                />
-              </IconContext.Provider>
-            }
-            className={`bg-white px-6 py-2 mb-6 
-            ${multipleOpen.includes(index) ? 'shadow-black-sm' : ''}`}
+          <AccordionHeader
+            onClick={() => handleToggleOpen(index)}
+            className={`heading-3 font-outfit text-left !font-medium !border-none 
+              ${openStates[index] ? '!text-palatinate-blue' : '!text-coarse-wool'}`}
           >
-            <AccordionHeader
-              onClick={() => handleMultipleOpen(index)}
-              className={`heading-3 font-outfit text-left !font-medium !border-none 
-              ${
-                multipleOpen.includes(index)
-                  ? '!text-palatinate-blue'
-                  : '!text-coarse-wool'
-              }`}
-            >
-              {faq.question}
-            </AccordionHeader>
+            {faq.question}
+          </AccordionHeader>
 
-            <AccordionBody className="text-brave-purple !pt-2 pb-6 font-outfit font-light text-lg leading-9">
-              {faq.answer}
-            </AccordionBody>
-          </RCAccordion>
-        </motion.div>
+          <AccordionBody className="text-brave-purple !pt-2 pb-6 font-outfit font-light text-lg leading-9">
+            {faq.answer}
+          </AccordionBody>
+        </RCAccordion>
       ))}
     </>
   );
