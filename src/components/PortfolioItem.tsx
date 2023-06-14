4ebd53687599d193152209e76ee582e2f5d8e937
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-06-14 10:56:57 +0800

    refactor: extract PortfolioItemProps, destructure portfolio props, simplify the render code to use isPortrait on rendering logic

--- src/components/PortfolioItem.tsx
+++ src/components/PortfolioItem.tsx
@@ -1,101 +1,57 @@
-import React from 'react';
-import { motion, Variants } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { transformVariants, commonMotionProps } from '@utils/motion';
 import type { PortfolioProps } from 'types/Portfolio';
 import Image from 'next/image';
 
-export default function PortfolioItem({
-  portfolio,
-  index,
-}: {
+type PortfolioItemProps = {
   portfolio: PortfolioProps;
   index?: number;
-}) {
-  const portfolioItemVariants: Variants = {
-    hidden: { y: 120, opacity: 0 },
-    visible: (i) => ({
-      opacity: 1,
-      y: 0,
-      transition: {
-        delay: i * 0.3,
-        duration: 0.3,
-        ease: 'linear',
-      },
-    }),
-  };
+};
 
-  return portfolio.orientation === 'potrait' ? (
+export default function PortfolioItem({ portfolio, index }: PortfolioItemProps) {
+  const {
+    imageId,
+    orientation,
+    projectName,
+    service: { name: serviceName },
+  } = portfolio;
+
+  const isPortrait = orientation === 'portrait';
+
+  return (
     <motion.div
-      initial="hidden"
-      whileInView="visible"
-      variants={portfolioItemVariants}
-      viewport={{ once: true }}
+      {...commonMotionProps}
+      variants={transformVariants('linear')}
       custom={index}
-      key={portfolio.id}
-      className="rounded-lg relative md:col-span-6 col-span-12 xl:col-span-4 row-span-4 overflow-hidden group"
+      className={`rounded-lg relative md:col-span-6 col-span-12 xl:col-span-4 overflow-hidden group ${
+        isPortrait ? 'row-span-4' : 'row-span-2'
+      }`}
     >
       <Image
-        src={portfolio.imageId}
-        alt={portfolio.projectName}
+        src={imageId}
+        alt={projectName}
         width={400}
-        height={600}
-        className="h-full w-full object-cover group-hover:scale-105 transition-all"
-      />
-
-      <div
-        className="absolute h-auto w-auto top-0 bottom-0 right-0 left-0 bg-gradient-to-tfrom-black
-      to-blue-gray-800 opacity-40 backdrop-blur-md"
-      />
-
-      <div className="absolute bottom-6 group-hover:bottom-8 px-6 transition-all delay-100">
-        <h3
-          className="text-2xl text-white font-medium leading-snug mb-2"
-          style={{ textShadow: '0 5px 10px rgba(0, 0, 0, .2)' }}
-        >
-          {portfolio.projectName}
-        </h3>
-        <p
-          className="text-white font-normal text-lg"
-          style={{ textShadow: '0 5px 10px rgba(0, 0, 0, .2)' }}
-        >
-          {portfolio.service.category}
-        </p>
-      </div>
-    </motion.div>
-  ) : (
-    <motion.div
-      initial="hidden"
-      whileInView="visible"
-      variants={portfolioItemVariants}
-      viewport={{ once: true }}
-      custom={index}
-      key={portfolio.id}
-      className="rounded-lg md:col-span-6 col-span-12 xl:col-span-4 relative row-span-2 overflow-hidden group"
-    >
-      <Image
-        src={portfolio.imageId}
-        alt={portfolio.projectName}
-        width={400}
-        height={200}
+        height={isPortrait ? 600 : 400}
         className="h-full w-full object-cover group-hover:scale-105 transition-all"
       />
 
       <div
         className="absolute h-auto w-auto top-0 bottom-0 right-0 left-0 bg-gradient-to-t from-black
-    to-blue-gray-800 opacity-40 backdrop-blur-md"
+      to-blue-gray-800 opacity-40 backdrop-blur-md"
       />
 
       <div className="absolute bottom-6 group-hover:bottom-8 px-6 transition-all delay-100">
         <h3
-          className="text-xl text-white font-medium leading-snug mb-2"
+          className={`${isPortrait ? 'heading-3' : 'heading-4'}  !text-white font-medium mb-2`}
           style={{ textShadow: '0 5px 10px rgba(0, 0, 0, .2)' }}
         >
-          {portfolio.projectName}
+          {projectName}
         </h3>
         <p
-          className="text-white font-normal text-base delay-100"
+          className={`!text-white font-normal ${isPortrait ? 'body-1' : 'body-2'}`}
           style={{ textShadow: '0 5px 10px rgba(0, 0, 0, .2)' }}
         >
-          {portfolio.service.category}
+          {serviceName}
         </p>
       </div>
     </motion.div>
