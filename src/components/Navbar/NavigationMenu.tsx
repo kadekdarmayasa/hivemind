8381907f9bad7d960c29f022737732fa81136970
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-06-14 12:09:55 +0800

    refactor: update import grouping, extract NavigationMenuProps and extract mobileMenuAnimation

--- src/components/Navbar/NavigationMenu.tsx
+++ src/components/Navbar/NavigationMenu.tsx
@@ -1,10 +1,10 @@
 import { useRef, useEffect, Dispatch, SetStateAction } from 'react';
-import type { NavItemProps } from 'types/NavItem';
-import Button from '@components/Button.tsx';
 import { IconContext } from 'react-icons';
-import { useMenuAnimation } from '@hooks/useMenuAnimation.tsx';
 import { useInView } from 'framer-motion';
 import { IoCloseCircleOutline } from 'react-icons/io5';
+import type { NavItemProps } from 'types/NavItem';
+import { useMenuAnimation } from '@hooks/useMenuAnimation';
+import Button from '@components/Button.tsx';
 import NavigationMenuItem from './NavigationMenuItem';
 
 type NavigationMenuProps = {
@@ -18,14 +18,15 @@ const closeIconProps: IconContext = {
   className: 'text-white group-hover:text-palatinate-blue transition-all',
 };
 
-export default function NavigationMenu({
-  isOpen,
-  setIsOpen,
-  menus,
-}: NavigationMenuProps): JSX.Element {
+export default function NavigationMenu(props: NavigationMenuProps) {
+  const { isOpen, setIsOpen, menus } = props;
   const ref = useRef(null);
   const isInView = useInView(ref);
   const { scope, setIsLastMenuItemInView } = useMenuAnimation(isOpen);
+
+  const mobileMenuAnimation = isOpen
+    ? 'bottom-0 md:bottom-auto opacity-100 md:opacity-100'
+    : 'bottom-full md:bottom-auto opacity-0 md:opacity-100';
 
   useEffect(() => {
     setIsLastMenuItemInView(isInView);
@@ -34,11 +35,7 @@ export default function NavigationMenu({
   return (
     <ul
       ref={scope}
-      className={`flex justify-center items-center flex-col md:flex-row fixed md:static top-0 md:top-auto left-0 md:left-auto right-0 md:right-auto  ${
-        isOpen
-          ? 'bottom-0 md:bottom-auto opacity-100 md:opacity-100'
-          : 'bottom-full md:bottom-auto opacity-0 md:opacity-100'
-      } bg-coarse-wool/90 md:bg-transparent z-10 w-full md:w-auto px-12 md:px-0 overflow-hidden md:overflow-visible transition-all duration-300`}
+      className={`flex justify-center items-center flex-col md:flex-row fixed md:static top-0 md:top-auto left-0 md:left-auto right-0 md:right-auto ${mobileMenuAnimation} bg-coarse-wool/90 md:bg-transparent z-10 w-full md:w-auto px-12 md:px-0 overflow-hidden md:overflow-visible transition-all duration-300`}
     >
       {menus.map((menu, i) => (
         <NavigationMenuItem
