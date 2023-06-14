229981a2bd8ae0a1205230510061ee8aa7e7160f
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-06-14 09:55:21 +0800

    test: update test message, and use toHaveAttribute assertion instead of toHaveProperty

--- src/__tests__/Button.test.tsx
+++ src/__tests__/Button.test.tsx
@@ -1,26 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import Button from '@components/Button';
 
-test('Button Should has background of palatinate blue when it is primary', () => {
+test('should have a background of palatinate blue when it is primary', () => {
   render(<Button isPrimary />);
 
   expect(screen.getByRole('button')).toHaveClass('bg-palatinate-blue');
 });
 
-test('Link Should has background of palatinate blue when it is primary', () => {
+test('should have a background of palatinate blue when it is primary link', () => {
   render(<Button isPrimary type="link" href="/" />);
 
   expect(screen.getByRole('link')).toHaveClass('bg-palatinate-blue');
 });
 
-test('Button should has type submit', () => {
+test('should have type submit', () => {
   render(<Button isPrimary type="submit" />);
 
-  expect(screen.getByRole('button')).toHaveProperty('type', 'submit');
+  expect(screen.getByRole('button')).toHaveAttribute('type', 'submit');
 });
 
-test('Button should has type button', () => {
-  render(<Button isPrimary />);
+test('should have type button by default', () => {
+  render(<Button />);
 
-  expect(screen.getByRole('button')).toHaveProperty('type', 'button');
+  expect(screen.getByRole('button')).toHaveAttribute('type', 'button');
 });
