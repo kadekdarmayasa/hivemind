8d41551b3c67a3ccbed06b126eeb5a6213b6a246
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-06-14 09:01:02 +0800

    refactor: add framer motion for each items of faq components

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { transformVariants, commonMotionProps } from '@utils/motion';
 import { Accordion as RCAccordion, AccordionHeader, AccordionBody } from '@material-tailwind/react';
 import { IconContext } from 'react-icons';
 import { IoChevronDown } from 'react-icons/io5';
@@ -18,29 +20,32 @@ export default function Accordion({ faqs }: { faqs: FAQProps[] }): JSX.Element {
   return (
     <>
       {faqs.map((faq, index) => (
-        <RCAccordion
-          open={openStates[index]}
-          data-testid="accordion-item"
-          icon={
-            <IconContext.Provider value={iconChevronDownProps}>
-              <IoChevronDown className={`${openStates[index] ? 'rotate-180' : ''}`} />
-            </IconContext.Provider>
-          }
-          className={`bg-white px-6 py-2 mb-6 
+        <motion.div key={faq.id} {...commonMotionProps} variants={transformVariants('linear')}>
+          <RCAccordion
+            key={faq.id}
+            open={openStates[index]}
+            data-testid="accordion-item"
+            icon={
+              <IconContext.Provider value={iconChevronDownProps}>
+                <IoChevronDown className={`${openStates[index] ? 'rotate-180' : ''}`} />
+              </IconContext.Provider>
+            }
+            className={`bg-white px-6 py-2 mb-6 
             ${openStates[index] ? 'shadow-black-sm' : ''}`}
-        >
-          <AccordionHeader
-            onClick={() => handleToggleOpen(index)}
-            className={`heading-3 font-outfit text-left !font-medium !border-none 
-              ${openStates[index] ? '!text-palatinate-blue' : '!text-coarse-wool'}`}
           >
-            {faq.question}
-          </AccordionHeader>
+            <AccordionHeader
+              onClick={() => handleToggleOpen(index)}
+              className={`heading-3 font-outfit text-left !font-medium !border-none 
+              ${openStates[index] ? '!text-palatinate-blue' : '!text-coarse-wool'}`}
+            >
+              {faq.question}
+            </AccordionHeader>
 
-          <AccordionBody className="text-brave-purple !pt-2 pb-6 font-outfit font-light text-lg leading-9">
-            {faq.answer}
-          </AccordionBody>
-        </RCAccordion>
+            <AccordionBody className="text-brave-purple !pt-2 pb-6 font-outfit font-light text-lg leading-9">
+              {faq.answer}
+            </AccordionBody>
+          </RCAccordion>
+        </motion.div>
       ))}
     </>
   );
