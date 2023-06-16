cce6d18a321ff047107a4f9cfe58df4b8b92770e
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-06-16 09:37:06 +0800

    feat: add loading animation when page load

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div id="loader-wrapper">
+      <div id="loader" />
+      <div className="loader-section section-left" />
+      <div className="loader-section section-right" />
+    </div>
+  );
+}
