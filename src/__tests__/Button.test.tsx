6f860f068c7dc14415051e07a2c55a61583be02e
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-06-23 20:10:12 +0800

    refactor:  update testing code for clarity and remove link render of button component

--- src/__tests__/Button.test.tsx
+++ src/__tests__/Button.test.tsx
@@ -1,20 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import Button from '@components/Button';
 
-test('should have a background of palatinate blue when it is primary', () => {
+test('should have a classname of "bg-palatinate-blue" when it is primary', () => {
   render(<Button isPrimary />);
 
   expect(screen.getByRole('button')).toHaveClass('bg-palatinate-blue');
 });
 
-test('should have a background of palatinate blue when it is primary link', () => {
-  render(<Button isPrimary type="link" href="/" />);
-
-  expect(screen.getByRole('link')).toHaveClass('bg-palatinate-blue');
-});
-
 test('should have type submit', () => {
-  render(<Button isPrimary type="submit" />);
+  render(<Button type="submit" />);
 
   expect(screen.getByRole('button')).toHaveAttribute('type', 'submit');
 });
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,52 +1,23 @@
-import Link from 'next/link';
 import classNames from 'classnames';
 
 type ButtonProps = {
   className?: string;
   onClick?: () => void;
-  type?: 'button' | 'submit' | 'link';
-  isExternal?: boolean;
+  type?: 'button' | 'submit';
   isPrimary?: boolean;
   disabled?: boolean;
-  href?: string;
   children?: React.ReactNode;
 };
 
 export default function Button(props: ButtonProps) {
-  const {
-    className,
-    onClick,
-    type = 'button',
-    isExternal,
-    isPrimary,
-    href,
-    children,
-    disabled,
-  } = props;
+  const { className, onClick, type = 'button', isPrimary, children, disabled } = props;
 
   const buttonClassNames = classNames(className, {
     'flex justify-center items-center': true,
     'bg-palatinate-blue shadow-purple-sm text-white': isPrimary,
     'text-coarse-wool': !isPrimary && type === 'button' && !disabled,
-    'bg-transparent text-palatinate-blue': !isPrimary && type === 'link',
     'bg-palatinate-blue text-white opacity-75 cursor-wait': !isPrimary && disabled,
   });
-
-  if (type === 'link' && isExternal) {
-    return (
-      <a href={href} target="_blank" rel="noreferrer" className={`${buttonClassNames}`}>
-        {children}
-      </a>
-    );
-  }
-
-  if (type === 'link' && !isExternal) {
-    return (
-      <Link tabIndex={0} href={href} className={`${buttonClassNames}`}>
-        {children}
-      </Link>
-    );
-  }
 
   return (
     <button
