1af7991d55e62ab23b2b28147b4e832633bba24a
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-06-24 20:48:47 +0800

    refactor test code for Navigation Menu

--- src/__tests__/NavigationMenu.test.tsx
+++ src/__tests__/NavigationMenu.test.tsx
@@ -5,19 +5,45 @@ import NavigationMenu from '@components/Navbar/NavigationMenuItem';
 // eslint-disable-next-line global-require
 jest.mock('next/router', () => require('next-router-mock'));
 
-test('li element should have class "active" when its child link is clicked', () => {
+test('first nav item should has classname "active" when the first render', () => {
   mockRouter.push('/');
 
+  render(<NavigationMenu path="/" name="Home" />);
   render(<NavigationMenu path="/about" name="About" />);
 
-  const navMenuContainer = screen.getByRole('listitem');
-  const navMenuLink = getByRole(navMenuContainer, 'link');
+  const navMenuItems = screen.getAllByRole('listitem');
 
-  expect(navMenuContainer).toBeInTheDocument();
-  expect(navMenuLink).toHaveTextContent('About');
+  expect(navMenuItems[0]).toHaveClass('active');
+  expect(mockRouter).toMatchObject({ asPath: '/' });
+});
+
+test('nav item should still has classname "active" if its child link is click twice or more', () => {
+  mockRouter.push('/');
+
+  render(<NavigationMenu path="/" name="Home" />);
+
+  const navMenuItem = screen.getByRole('listitem');
+  const navMenuLink = getByRole(navMenuItem, 'link');
 
   fireEvent.click(navMenuLink);
+  fireEvent.click(navMenuLink);
 
-  expect(navMenuContainer).toHaveClass('active');
+  expect(navMenuItem).toHaveClass('active');
+  expect(mockRouter).toMatchObject({ asPath: '/' });
+});
+
+test('should only one nav item that has classname "active"', () => {
+  mockRouter.push('/');
+
+  render(<NavigationMenu path="/" name="Home" />);
+  render(<NavigationMenu path="/about" name="About" />);
+
+  const navMenuItems = screen.getAllByRole('listitem');
+  const navMenuLinks = screen.getAllByRole('link');
+
+  fireEvent.click(navMenuLinks[1]);
+
+  expect(navMenuItems[0]).not.toHaveClass('active');
+  expect(navMenuItems[1]).toHaveClass('active');
   expect(mockRouter).toMatchObject({ asPath: '/about' });
 });
