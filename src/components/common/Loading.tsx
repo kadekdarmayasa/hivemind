58def4913885d0732c6cf885f70e343a0cbc3b65
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-07-09 10:38:59 +0800

    update loading indicator

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image';
+import hivemindLogo from '../../../public/images/hivemind-logo.svg';
+
 export default function Loading() {
   return (
-    <div id="loader-wrapper">
-      <div id="loader" />
-      <div className="loader-section section-left" />
-      <div className="loader-section section-right" />
+    <div className="h-screen w-screen bg-ghost-white flex justify-center items-center">
+      <Image
+        src={hivemindLogo}
+        height={80}
+        alt="Hivemind Logo"
+        className="mr-5 mt-2 logo-hivemind--animate"
+      />
+      <h1 className="heading-1 !text-palatinate-blue">Hivemind</h1>
     </div>
   );
 }
